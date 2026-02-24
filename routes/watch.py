f4a2b92beacfba8336ef8210cb9ea96ae268b230
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-25 05:13:05 +0530

    Update watch.py

--- routes/watch.py
+++ routes/watch.py
@@ -1,29 +1,42 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import StreamingResponse
 from services.Yt_Service import get_stream_urls
 from YouTubeMusic.Video_Stream import start_ffmpeg_merge
+import asyncio
 
 router = APIRouter()
 
 @router.get("/")
-async def watch(url: str = Query(...)):
+async def watch(request: Request, url: str):
 
     video_url, audio_url = get_stream_urls(url)
 
     if not video_url:
-        raise HTTPException(404, "Stream not found")
+        raise HTTPException(400, "Invalid YouTube URL")
 
     process = start_ffmpeg_merge(video_url, audio_url)
 
-    async def generator():
+    async def stream_generator():
         try:
             while True:
-                chunk = process.stdout.read(1024 * 1024)
+                # Client disconnected?
+                if await request.is_disconnected():
+                    break
+
+                chunk = await asyncio.to_thread(
+                    process.stdout.read, 1024 * 512
+                )
+
                 if not chunk:
                     break
+
                 yield chunk
+
         finally:
             process.kill()
             process.wait()
 
-    return StreamingResponse(generator(), media_type="video/mp4")
+    return StreamingResponse(
+        stream_generator(),
+        media_type="video/mp4"
+    )
