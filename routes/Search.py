633e16269637ea3fdfaac576a8ed98733efc9438
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-25 05:21:15 +0530

    Update Search.py

--- routes/Search.py
+++ routes/Search.py
@@ -6,3 +6,7 @@ router = APIRouter()
 @router.get("/")
 async def search(q: str):
     return await search_youtube(q)
+
+@router.get("/suggest")
+async def suggest(title: str, url: str):
+    return await get_suggestions(title, url)
