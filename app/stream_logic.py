3a184bed73de6f1b01d004235ff82a315c05cfc9
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-25 02:46:49 +0530

    Update stream_logic.py

--- app/stream_logic.py
+++ app/stream_logic.py
@@ -2,7 +2,7 @@ from YouTubeMusic.Stream import get_stream
 
 async def ytdl_audio(url: str):
     try:
-        stream_url = get_stream(url)
+        stream_url = get_stream(url, "/app/cookies.txt")
         if not stream_url:
             return False, "Failed to get audio stream URL"
         return True, stream_url
